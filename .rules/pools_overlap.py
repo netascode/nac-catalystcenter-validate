e14c39383f2b6f4b221a52ef882b22c623dcf57c
kuba-mazurkiewicz <kmazurki@cisco.com>
2025-01-21 22:08:59 +0100

    add pool overlap

---
+++ .rules/pools_overlap.py
@@ -0,0 +1,24 @@
+import ipaddress
+
+class Rule:
+    id = "104"
+    description = "Verify IP Pool subnet overlap"
+    severity = "HIGH"
+
+    @classmethod
+    def match(cls, data):
+        results = []
+        try:
+            pools_subnets = []
+            for pool in data["catalyst_center"]["network_settings"]["ip_pools"]:
+                print(pool)
+                pools_subnets.append(ipaddress.ip_network(pool["ip_pool_cidr"], strict=False))
+            for idx, subnet in enumerate(pools_subnets):
+                if idx + 1 >= len(pools_subnets):
+                    break
+                for other_subnet in pools_subnets[idx + 1 :]:
+                    if subnet.overlaps(other_subnet):
+                        results.append("catalyst_center.network_settings.ip_pools.ip_pool_cidr: {} - {}".format(subnet, pool['name'])) 
+        except KeyError:
+            pass
+        return results
